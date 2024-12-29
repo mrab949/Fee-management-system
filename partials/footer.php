ec75a75b4c30b02ae1a721e188b68391b9f2e9be
Muhammad Rizwan <157191553+mrab949@users.noreply.github.com>
2024-12-29 15:22:03 +0500

    Create footer.php

---
+++ partials/footer.php
@@ -0,0 +1,34 @@
+
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../assets/footer.css">
+
+    <title>Footer</title>
+
+</head>
+
+<body>
+
+    <footer class="footer">
+
+        <p>Developed by students of computer engineering BZU Multan - All rights reserved</p><br>
+
+        <div><pre><a href="../private_admin/help.php" style="color:grey;">Terms and Conditions </a>|<a href="../private_admin/help.php" style="color:grey;"> Privacy polcy</a></pre></div>
+
+    </footer>
+
+</body>
+
+</html>
+
+
+
